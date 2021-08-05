1164492c42d7a4ee2b22fc76bb55beb99ed09d06
thong <thonghuynh1711@gmail.com>
2021-08-05 10:39:16 +0700

    first commit

---
+++ view/main/hethongcuahang.php
@@ -0,0 +1,17 @@
+<div class="container-fluid">
+	<div class="container">
+		<div class="row boderbot">
+			<h3 class="col-md-10 ft"> <?php echo mb_strtoupper('hệ thốn cửa hàng'); ?> </h3>
+		</div><br>
+		<div class="row">
+			<div class=" m-auto">
+				<img src="webroot/img/2.jpg" width="200" height="350">
+
+			</div>
+			
+				
+		</div>
+		<div class="text-center"><i class="fas fa-map-marker-alt"></i> Số 451 Hoàng Diệu - Thành phố Đà Nẵng</div>
+	</div>
+	
+</div>
